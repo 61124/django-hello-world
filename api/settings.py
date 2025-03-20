d93f4489e982e892cb668406c7895c39417b9dd9
61124 <onlyad.26@gmail.com>
2025-03-20 13:45:50 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-if os.environ.get("CREATE_SUPERUSER"):
-    from django.contrib.auth.models import User
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "password123")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
