0db7f50229123c7423cd55607c37ba6f70d3ced1
61124 <onlyad.26@gmail.com>
2025-03-20 13:54:30 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 # example/urls.py
 from django.urls import path
-from example.views import index, contact, contact_success
+from example.views import index, contact, contact_success, submission_list, submission_detail
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('contact/success/', contact_success, name='contact_success'),
+    path('submissions/', submission_list, name='submission_list'),
+    path('submissions/<int:submission_id>/', submission_detail, name='submission_detail'),
 ]
